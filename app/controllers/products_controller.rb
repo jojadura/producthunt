038983760847d7d0b6a041eb66888fc1bc808d11
John Jairo <jojadura@hotmail.com>
2015-11-16 16:58:01 -0500

    Crea la ruta POST /products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,21 @@ class ProductsController < ApplicationController
 	def new
 		@product = Product.new
 	end
+
+	def create
+		@product = Product.new(products_params)
+		if @product.save
+			redirect_to products_path, notice: "El producto fue publicado con éxito"
+		else
+			render :new
+		end
+		
+	end
+
+	private 
+		def products_params
+			params.require(:product).permit(:name, :url, :description)
+			
+		end
+
 end
