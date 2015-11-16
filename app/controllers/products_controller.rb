ce9dc3150c0331ceb6fadcaaad70e31f50429e41
John Jairo <jojadura@hotmail.com>
2015-11-16 16:38:24 -0500

    Crea la ruta /products/new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,8 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all  #el @ nos va permitir acceder a esa variable desde la vista		
 	end
+
+	def new
+		@product = Product.new
+	end
 end
