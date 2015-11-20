156e58a1c8efdc361e57b6d0ffb46e5b04abad8b
john j <as@gmail.com>
2015-11-20 16:24:06 -0500

    Asociar Productos a Usuario

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,9 +8,12 @@
 #  description :text
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer
 #
 
 class Product < ActiveRecord::Base
+
+	belongs_to :user  #Agregarle un atributo a los productos para poder acceder al usuario, y con ese obtener los datos de ese usuario.
 	
 	validates :name, presence: true
 	validates :url, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119141844) do
+ActiveRecord::Schema.define(version: 20151120210243) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20151119141844) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",           limit: 100
