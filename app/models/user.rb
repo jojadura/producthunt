074305660d33eeb9f79723eaf6594400fc3d966e
john j <as@gmail.com>
2015-11-20 15:19:15 -0500

    Agrega validaciones a User

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,10 @@
 class User < ActiveRecord::Base
 
 	has_secure_password validations: false  #para que quede cifrado ese password.
+
+	validates :email, uniqueness: true, format: /@/
+	validates :password, presence: true, on: :create
+	validates :password, length: { in: 6..20 }, allow_nil: true  #nil, porque cuando se editando el usuario este dato no va a llegar
+	validates :name, presence: true	  # presence: true, significa que debe existir información en ese campo
+
 end
